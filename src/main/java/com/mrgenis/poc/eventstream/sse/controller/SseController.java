1ea64482f66dd97bac0040bdd7b33e0ad3e82045
SergioFloresG <sf.cancun@gmail.com>
2024-09-25 00:29:13 -0600

    Change GET to POST for stream endpoint
    Replaced @GetMapping with @PostMapping in SseController to handle POST requests with a request body. Modified the streamEvent method to accept a JSON body and create a list of dynamic JSON objects based on random generation.

--- src/main/java/com/mrgenis/poc/eventstream/sse/controller/SseController.java
+++ src/main/java/com/mrgenis/poc/eventstream/sse/controller/SseController.java
@@ -1,17 +1,21 @@
 package com.mrgenis.poc.eventstream.sse.controller;
 
 import com.mrgenis.poc.eventstream.sse.controller.response.StreamResponse;
+import com.mrgenis.poc.eventstream.sse.mapper.StringToDynamicJsonObject;
 import com.mrgenis.poc.eventstream.sse.mapper.ToServerSentEvent;
 import com.mrgenis.poc.eventstream.sse.mapper.ToStreamResponseMapper;
 import com.mrgenis.poc.eventstream.sse.usecase.LoremUseCase;
 import java.time.Duration;
+import java.util.Collections;
 import java.util.List;
 import java.util.function.Supplier;
 import lombok.RequiredArgsConstructor;
+import org.json.JSONObject;
 import org.springframework.http.MediaType;
 import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -22,13 +26,19 @@ public class SseController {
 
   private final LoremUseCase loremUseCase;
   private final ToServerSentEvent<String> toStreamMapper;
+  private final StringToDynamicJsonObject stringToDynamicJsonObject;
 
   @CrossOrigin(origins = "*")
-  @GetMapping(path = "/stream-sse", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-  public Flux<ServerSentEvent<StreamResponse<String>>> streamEvent() {
-    List<String> messages = loremUseCase.getLoremChunks();
+  @PostMapping(path = "/stream-sse", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+  public Flux<ServerSentEvent<StreamResponse<String>>> streamEvent(@RequestBody String body) {
 
-    var mapper = new ToStreamResponseMapper<String>();
+    int randomNum = (int) (Math.random() * 2) + 5;
+    JSONObject jsonObject = stringToDynamicJsonObject.apply(body);
+
+    /*List<String> messages =  loremUseCase.getLoremChunks();*/
+    List<Object> messages = Collections.nCopies(randomNum, jsonObject);
+
+    var mapper = new ToStreamResponseMapper<Object>();
     var toSSE = mapper.andThen(toStreamMapper);
 
     Supplier<ServerSentEvent<StreamResponse<String>>> lastMessage = () -> {
